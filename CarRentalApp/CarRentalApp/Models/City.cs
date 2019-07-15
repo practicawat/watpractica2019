e08b23a8a3c88b22445845b07adafbec4cc6de58
Florina <heroiuflorina@gmail.com>
2019-07-15 12:12:22 +0300

    Add class City.

---
+++ CarRentalApp/CarRentalApp/Models/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Models
+{
+    public class City
+    {
+        public int IDCity { get; private set; }
+        public string CityName { get; private set; }
+        public string CountyName { get; private set; }
+
+        public City(int id, string nameCity, string nameCounty)
+        {
+            this.IDCity = id;
+            this.CityName = nameCity;
+            this.CountyName = nameCounty;
+        }
+    }
+}
