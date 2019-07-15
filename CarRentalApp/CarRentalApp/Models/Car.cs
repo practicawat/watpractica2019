06a0d92af7085038bb8747762398f6beed8dbf94
hazeflame <stanciu.d.vlad47@gmail.com>
2019-07-15 11:42:59 +0300

    Created Car Class

--- CarRentalApp/CarRentalApp/Models/Car.cs
+++ CarRentalApp/CarRentalApp/Models/Car.cs
@@ -2,10 +2,26 @@
 {
     public class Car
     {
-        public int CarId { get; set; }
-        public string Name { get; set; }        
-        public string CompanyName { get; set; }
-        public string Designation { get; set; }
-        public float Price { get; set; }
+        
+            private string brand { get; set; }
+            private string model { get; set; }
+            private short nrOfDoors { get; set; }
+            private string licensePlate { get; set; }
+            private short nrOfSeats { get; set; }
+            private bool hasAutomaticGearbox { get; set; }
+            private float pricePerDay { get; set; }
+
+            public Car(string brand, string model, short nrOfDoors, string licensePlaye, short nrOfSeats, bool hasAutomaticGearbox, float pricePerDay)
+            {
+                this.brand = brand;
+                this.model = model;
+                this.nrOfDoors = nrOfDoors;
+                this.licensePlate = licensePlate;
+                this.nrOfSeats = nrOfSeats;
+                this.hasAutomaticGearbox = hasAutomaticGearbox;
+                this.pricePerDay = pricePerDay;
+            }
+
+        
     }
 }
