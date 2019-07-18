47669ab40a0acec189cdfbbb70d2057350785072
hazeflame <stanciu.d.vlad47@gmail.com>
2019-07-18 14:20:01 +0300

    Bottom numbers change on click

--- CarRentalApp/CarRentalApp/ClientApp/src/app/components/car-table/car-table.component.ts
+++ CarRentalApp/CarRentalApp/ClientApp/src/app/components/car-table/car-table.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService } from 'src/app/services/cars.service';
+import { findLast } from '@angular/compiler/src/directive_resolver';
+export class PageState{
+  constructor(
+    public firstButton: number,
+    public middleButton: number,
+    public leftButton: number,
+
+    public isFirstButtonSelected :boolean,
+    public isMiddleButtonSelected :boolean,
+    public isLeftButtonSelected :boolean,
+  ){}
+}
+
+
 
 @Component({
   selector: 'app-car-table',
@@ -8,8 +22,20 @@ import { CarService } from 'src/app/services/cars.service';
 })
 export class CarTableComponent implements OnInit {
   public cars = [];
+  public pageState :PageState;
+  public leftClassManager = {}
+  public middleClassManager = {}
+  public rightClassManager = {}
 
-  constructor(private _carService: CarService) { }
+
+
+
+  constructor(private _carService: CarService) {
+   this.pageState = new PageState(
+     1,2,3,true,false,false)
+    this.initiatePageManagers();
+   }
+
 
   ngOnInit() {
     this._carService.getAllCars()
@@ -18,8 +44,53 @@ export class CarTableComponent implements OnInit {
   }
 
 
+  initiatePageManagers = () =>{
+    this.leftClassManager = {
+      "btn" : true,
+      "btn-primary" : this.pageState.isFirstButtonSelected,
+      "btn-default" : !this.pageState.isFirstButtonSelected,
+    }
+    this.middleClassManager = {
+      "btn" : true,
+      "btn-primary" : this.pageState.isMiddleButtonSelected,
+      "btn-default" : !this.pageState.isMiddleButtonSelected,
+    }
+    this.rightClassManager = {
+      "btn" : true,
+      "btn-primary" : this.pageState.isLeftButtonSelected,
+      "btn-default" : !this.pageState.isLeftButtonSelected,
+    }
+  }
+
+
   testData = (event) =>{
     console.log(this.cars);
+    console.log(Object.keys(this.cars).length);
+    console.log(this.pageState.firstButton);
+  }
+
+  changePage = (event) =>{
+    var target = event.target || event.srcElement || event.currentTarget
+
+    console.log(target.attributes.id.value)
+    if(target.attributes.id.value ==="middle"){
+      this.pageState.isFirstButtonSelected = false;
+      this.pageState.isMiddleButtonSelected = true;
+      this.pageState.isLeftButtonSelected = false;
+      this.initiatePageManagers()
+    }
+    if(target.attributes.id.value ==="first"){
+      this.pageState.isFirstButtonSelected = true;
+      this.pageState.isMiddleButtonSelected = false;
+      this.pageState.isLeftButtonSelected = false;
+      this.initiatePageManagers()
+    }
+    if(target.attributes.id.value ==="right"){
+      this.pageState.isFirstButtonSelected = false;
+      this.pageState.isMiddleButtonSelected = false;
+      this.pageState.isLeftButtonSelected = true;
+      this.initiatePageManagers()
+    }
   }
 
 }
