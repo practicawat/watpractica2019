b547f88d67438bbecfa0ac090b8255bd3c195405
Filller <fil2001ip@gmail.com>
2019-07-25 14:21:31 +0200

    .

--- CarRentalApp/CarRentalApp/Migrations/20190725073049_mig.Designer.cs
+++ CarRentalApp/CarRentalApp/Migrations/20190725073049_mig.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRentalApp.Migrations
 {
     [DbContext(typeof(CarRentalDbContext))]
-    [Migration("20190722070605_NewMigration")]
-    partial class NewMigration
+    [Migration("20190725073049_mig")]
+    partial class mig
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
--- CarRentalApp/CarRentalApp/Migrations/20190725073049_mig.cs
+++ CarRentalApp/CarRentalApp/Migrations/20190725073049_mig.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace CarRentalApp.Migrations
 {
-    public partial class migrationAfterMerge : Migration
+    public partial class mig : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
