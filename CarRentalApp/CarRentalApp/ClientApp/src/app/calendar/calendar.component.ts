cd800d5a167d898606c1de2e4da5dfb15fed9f70
Emilia Cretu <Emilia Cretu@DESKTOP-AJC3LE4>
2019-07-25 09:25:07 +0300

    nada

--- CarRentalApp/CarRentalApp/ClientApp/src/app/calendar/calendar.component.ts
+++ CarRentalApp/CarRentalApp/ClientApp/src/app/calendar/calendar.component.ts
@@ -78,48 +78,24 @@ export class CalendarComponent {
 
   events: CalendarEvent[] = [
     {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
+      start: subDays(startOfDay(new Date()), 0),
+      end: addDays(new Date(), 2),
+      title: 'Rented',
+      
     },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      actions: this.actions
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: new Date(),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    }
   ];
 
   activeDayIsOpen: boolean = true;
 
-  constructor(private modal: NgbModal) {}
+  constructor(private modal: NgbModal) { }
+
+  ngAfterViewInit() {
+    var x = document.getElementsByClassName("cal-cell-top ")
+    var i;
+
+  } 
+
+
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (isSameMonth(date, this.viewDate)) {
@@ -138,7 +114,7 @@ export class CalendarComponent {
   eventTimesChanged({
     event,
     newStart,
-    newEnd
+    newEnd,
   }: CalendarEventTimesChangedEvent): void {
     this.events = this.events.map(iEvent => {
       if (iEvent === event) {
